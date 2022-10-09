31c61002c4939144a9333b4d0b71a48bed94cd54
gguilt <58526116+gguilt@users.noreply.github.com>
2022-10-09 16:47:22 +0300

    Initial upload

---
+++ src/App.vue
@@ -0,0 +1,52 @@
+<script>
+let id = 0
+
+export default {
+  data() {
+    return {
+      newTodo: '',
+      hideCompleted: false,
+      todos: [
+        { id: id++, text: 'Open To-do app', done: true },
+        { id: id++, text: 'Add new task', done: false }
+      ]
+    }
+  },
+  computed: {
+    filteredTodos() {
+      return this.hideCompleted
+        ? this.todos.filter((t) => !t.done)
+        : this.todos
+    }
+  },
+  methods: {
+    addTodo() {
+      this.todos.push({ id: id++, text: this.newTodo, done: false })
+      this.newTodo = ''
+    },
+    removeTodo(todo) {
+      this.todos = this.todos.filter((t) => t !== todo)
+    }
+  }
+}
+</script>
+
+<template>
+<h1>Furiyomi</h1>
+<input placeholder="Add your title">
+<br>
+<textarea placeholder="Add your main text"></textarea>
+<br>
+<button>Start editing!</button>
+</template>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+
+@import '~bootstrap/dist/css/bootstrap.css'
+</style>
